723e9b0e916864e23f203e19b85859c1158a2cff
Daniel-J101 <frostysausage123@gmail.com>
2024-01-28 17:51:43 -0600

    audio control continue

--- src/main/java/frc/spectrumLib/orchestra/AudioControl.java
+++ src/main/java/frc/spectrumLib/orchestra/AudioControl.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.hardware.ParentDevice;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Robot;
@@ -16,9 +17,13 @@ import java.util.stream.Stream;
 
 //TODO: separate tracks and SFX, implement audio control in robot control & gamepads
 public class AudioControl implements Subsystem {
-    public Orchestra orchestra;
-    public Collection<ParentDevice> instruments;
+    public Orchestra mixer;
+    public Orchestra sfxControl;
+    public Collection<ParentDevice> trackInstruments;
+    public Collection<ParentDevice> sfxInstruments;
 
+    private boolean runWhileEnabled = false;
+    private final String[] sfx = { "sfx1.chrp"};
     private final String[] playlist = {
         "track1.chrp",
         "track2.chrp",
@@ -38,86 +43,152 @@ public class AudioControl implements Subsystem {
         "track16.chrp",
         "track17.chrp",
         "track18.chrp",
-        "track19.chrp",
-        "track20.chrp"
+        "track19.chrp"
     };
     private int currentTrackNumber = 0;
     private String currentTrack = playlist[currentTrackNumber];
 
-    public AudioControl() {
+    /**
+     * 
+     * @param runWhileEnabled whether tracks (not SFX) run in enabled
+     */
+    public AudioControl(boolean runWhileEnabled) {
+        this.runWhileEnabled = runWhileEnabled;
+        // retrieve swerve modules
         Stream<frc.spectrumLib.swerve.Module> swerveModules =
-                Arrays.stream(Robot.swerve.getModules()); // retrieve swerve modules
-        instruments =
+                Arrays.stream(Robot.swerve.getModules()); 
+
+        // create sfx instruments
+        sfxInstruments =
                 Mechanism.getInstances().stream()
                         .map(Mechanism::getMotor)
-                        .collect(Collectors.toList()); // add mechanism motors
-        instruments.addAll(
+                        .collect(Collectors.toList()); // only mechanism motors
+
+        //create track instruments
+        trackInstruments.addAll(sfxInstruments); // include mechanism motors in track instruments
+        trackInstruments.addAll(
                 swerveModules
                         .flatMap(
                                 module -> Stream.of(module.getDriveMotor(), module.getSteerMotor()))
-                        .collect(Collectors.toList())); // add swerve drive + angle motors
-        orchestra = new Orchestra(instruments);
-        loadMusic(currentTrack); //queue up first song
+                        .collect(Collectors.toList())); // add swerve drive & angle motors
+
+        //create orchestras
+        sfxControl = new Orchestra(sfxInstruments);
+        mixer = new Orchestra(trackInstruments);
+
+        loadTrack(currentTrack); //queue up first song
+        loadSFX(sfx[0]); //queue up sfx
     }
 
-    /* Custom */
-    public void skipTrack() {
-        currentTrack = playlist[++currentTrackNumber];
+    /* Mixer Commands */
+    public Command runMixerPlay() {
+        return runOnce(() -> playTrack()).withName("AudioControl.play").ignoringDisable(true);
     }
 
-    /* Commands */
-    public Command runPlay() {
-        return runOnce(() -> play()).withName("AudioControl.play").ignoringDisable(true);
-    }
-
-    public Command runPause() {
+    public Command runMixerPause() {
         return runOnce(() -> pause()).withName("AudioControl.pause").ignoringDisable(true);
     }
 
-    public Command runSkip() {
+    public Command runMixerSkip() {
         return runOnce(() -> skipTrack()).withName("AudioControl.skip").ignoringDisable(true);
     }
 
-
-    /* Wrapper */
-
-    public StatusCode addInstrument(ParentDevice instrument) {
-        return orchestra.addInstrument(instrument);
+    /* SFX Commands */
+    public Command runSFXPlay() {
+        return runOnce(() -> playSFX()).withName("AudioControl.playSFX").ignoringDisable(true);
     }
 
-    public StatusCode addInstrument(ParentDevice instrument, int trackNumber) {
-        return orchestra.addInstrument(instrument, trackNumber);
+    public Command runSFXPlay(String filepath) {
+        return runOnce(() -> playSFX(filepath)).withName("AudioControl.playSFX").ignoringDisable(true);
     }
 
-    public StatusCode clearInstruments() {
-        return orchestra.clearInstruments();
+    public Command runSFXStop() {
+        return runOnce(() -> stopSFX()).withName("AudioControl.stopSFX").ignoringDisable(true);
     }
 
-    public void close() {
-        orchestra.close();
+
+    /* SFX Wrapper */
+
+    public void playSFX() {
+        playSFX(sfx[0]);
     }
 
-    public double getCurrentTime() {
-        return orchestra.getCurrentTime();
+    public void playSFX(String filepath) {
+        loadSFX(filepath);
+        sfxControl.play();
     }
 
-    public boolean isPlaying() {
-        return orchestra.isPlaying();
+    public StatusCode loadSFX(String filepath) {
+        return sfxControl.loadMusic("chirps/sfx/" + filepath);
     }
 
-    public StatusCode loadMusic(String filepath) {
-        return orchestra.loadMusic("chirps/" + filepath);
+    public StatusCode stopSFX() {
+        return sfxControl.stop();
+    }
+
+    /* Mixer Custom */
+
+    //stop current song and continue to next song in playlist
+    public void skipTrack() {
+        currentTrack = playlist[++currentTrackNumber];
+        loadTrack(currentTrack);
+    }
+
+    /* Mixer Wrapper */
+
+    public StatusCode addMixerInstrument(ParentDevice instrument) {
+        return mixer.addInstrument(instrument);
+    }
+
+    public StatusCode addMixerInstrument(ParentDevice instrument, int trackNumber) {
+        return mixer.addInstrument(instrument, trackNumber);
+    }
+
+    public StatusCode clearMixerInstruments() {
+        return mixer.clearInstruments();
+    }
+
+    public void closeMixer() {
+        mixer.close();
+    }
+
+    public double getCurrentTrackTime() {
+        return mixer.getCurrentTime();
+    }
+
+    public boolean isTrackPlaying() {
+        return mixer.isPlaying();
+    }
+
+    public StatusCode loadTrack(String filepath) {
+        return mixer.loadMusic("chirps/tracks/" + filepath);
     }
 
     public StatusCode pause() {
-        return orchestra.pause();
+        return mixer.pause();
     }
 
-    public StatusCode play() {
-        return orchestra.play();
+    public void playTrack() {
+        if(canRun()) {
+            mixer.play();
+        }
     }
 
+    //stop current song and continue to next song in playlist
     public StatusCode stop() {
-        return orchestra.stop();
+        StatusCode status = mixer.stop();
+        skipTrack();
+        return status;
+    }
+
+
+    /* Audio Control */
+    public boolean canRun() {
+        if(DriverStation.isEnabled()) {
+            if(!runWhileEnabled) {
+                return false;
+            }
+        }
+        return true;
     }
 }
