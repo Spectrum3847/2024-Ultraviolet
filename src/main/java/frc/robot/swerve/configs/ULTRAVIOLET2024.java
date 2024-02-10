5e52b9cd9a45ac4d87ccc1d69285b95d98dfc4c0
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2024-02-10 17:42:17 -0600

    gain changes revert + rough rotation controller gains

--- src/main/java/frc/robot/swerve/configs/ULTRAVIOLET2024.java
+++ src/main/java/frc/robot/swerve/configs/ULTRAVIOLET2024.java
@@ -10,10 +10,10 @@ import frc.spectrumLib.swerve.config.SwerveConfig;
 public class ULTRAVIOLET2024 {
 
     // Angle Offsets: from cancoder Absolute Position No Offset, opposite sign
-    private static final double kFrontLeftCANcoderOffset = 0.143799;
-    private static final double kFrontRightCANncoderOffset = -0.201172;
-    private static final double kBackLeftCANcoderOffset = 0.212158;
-    private static final double kBackRightCANcoderOffset = 0.279541;
+    private static final double kFrontLeftCANcoderOffset = -0.356689;
+    private static final double kFrontRightCANncoderOffset = -0.201660;
+    private static final double kBackLeftCANcoderOffset = -0.282715;
+    private static final double kBackRightCANcoderOffset = -0.220459;
 
     // Physical Config
     private static final double wheelBaseInches = 21.5;
@@ -23,15 +23,15 @@ public class ULTRAVIOLET2024 {
 
     // Tuning Config
     // Estimated at first, then fudge-factored to make odom match record
-    private static final double kWheelRadiusInches = 3.9 / 2;
+    private static final double kWheelRadiusInches = 2;
     private static final double speedAt12VoltsMps = 6;
+
     private static final double slipCurrent = 800;
-    private static final SlotGains steerGains = new SlotGains(80.42574, 0, 0, 0.2767262, 0);
-    private static final SlotGains driveGains =
-            new SlotGains(1.1200337829912024, 0, 0, 0.0028068, 3.2105);
+    private static final SlotGains steerGains = new SlotGains(100, 0, 0.05, 0, 0);
+    private static final SlotGains driveGains = new SlotGains(0.4, 0, 0, 0, 0);
 
     /*Rotation Controller*/
-    private static final double kPRotationController = 0.0;
+    private static final double kPRotationController = 7.0;
     private static final double kIRotationController = 0.0;
     private static final double kDRotationController = 0.0;
 
@@ -84,7 +84,8 @@ public class ULTRAVIOLET2024 {
                     .withDriveMotorGains(driveGains)
                     .withSteerMotorGains(steerGains)
                     .withWheelRadius(kWheelRadiusInches)
-                    .withFeedbackSource(steerFeedbackType);
+                    .withFeedbackSource(steerFeedbackType)
+                    .withDriveMotorInverted(false);
 
     public static final ModuleConfig BackLeft =
             DefaultConfig.BackLeft.withCANcoderOffset(kBackLeftCANcoderOffset)
