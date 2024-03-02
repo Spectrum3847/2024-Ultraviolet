212c4dc20d5436c4b26d74b080132402fa154b59
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2024-03-02 14:01:34 -0600

    small wheel update before p1

--- src/main/java/frc/robot/swerve/configs/PHOTON.java
+++ src/main/java/frc/robot/swerve/configs/PHOTON.java
@@ -10,10 +10,10 @@ import frc.spectrumLib.swerve.config.SwerveConfig;
 public class PHOTON {
 
     // Angle Offsets: from cancoder Absolute Position No Offset, opposite sign
-    private static final double kFrontLeftCANcoderOffset = 0.264648;
-    private static final double kFrontRightCANncoderOffset = 0.087646;
+    private static final double kFrontLeftCANcoderOffset = 0.265137;
+    private static final double kFrontRightCANncoderOffset = 0.089844;
     private static final double kBackLeftCANcoderOffset = -0.038818;
-    private static final double kBackRightCANcoderOffset = 0.486572;
+    private static final double kBackRightCANcoderOffset = 0.486084;
 
     // Physical Config
     private static final double wheelBaseInches = 21.5;
