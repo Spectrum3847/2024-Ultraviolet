b9e9f355d5624f104d5d38925432c0089313c2d3
Spectrum3847 <spectrum3847@gmail.com>
2024-06-28 16:33:39 -0500

    Updates before M29

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -271,6 +271,8 @@ public class Robot extends LoggedRobot {
                                     ? 0
                                     : 180));
 
+            vision.detectLL.setLimelightPipeline(2);
+
             // if(DriverStation.isFMSAttached()) {
             //     ClimberCommands.safeClimb().withTimeout(2).schedule();
             // }
